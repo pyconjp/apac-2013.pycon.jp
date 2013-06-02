8268d26d037d1c0171481f810d03ceeeae8d8e60
shoma hosaka <shoma.h4a+bitbucket@gmail.com>
2013-06-02 18:21:22 +0900

    - テーマ適用 - toctree は css で消した

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -98,7 +98,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 # html_theme = 'default'
-html_theme = 'theme'
+html_theme = 'pycon_apac_2013_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
